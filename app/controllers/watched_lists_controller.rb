532e89ae3bb05d671e0e09e67600a91679a16213
Rondy <rondysousa@gmail.com>
2011-11-29 19:16:54 -0200

    Refactored create/destroy actions on WatchedListsController

--- app/controllers/watched_lists_controller.rb
+++ app/controllers/watched_lists_controller.rb
@@ -1,12 +1,20 @@
 class WatchedListsController < ApplicationController
 
   def create
-    @todo_list = TodoList.public_visibility.find_by_id(params[:todo_list_id])
+    @todo_list = TodoList.public_visibility.find(params[:todo_list_id])
 
-    if @todo_list && current_user.watch!(@todo_list)
-      flash[:notice] = %{You are watching %s} % @todo_list.name
-    else
-      flash[:error] = %{Sorry, You can't watch %s} % @todo_list.name
+    if current_user.watch!(@todo_list)
+      flash[:notice] = %{You are watching "%s"} % @todo_list.name
+    end
+
+    redirect_to public_todo_lists_url
+  end
+
+  def destroy
+    @todo_list = TodoList.public_visibility.find_by_id(params[:id])
+
+    if current_user.unwatch!(@todo_list)
+      flash[:notice] = %{You are no longer watching "%s"} % @todo_list.name
     end
 
     redirect_to public_todo_lists_url
