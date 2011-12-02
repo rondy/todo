bb1838548f2e6790c94edd878ec6b62914768fa8
Rondy <rondysousa@gmail.com>
2011-12-02 20:27:00 -0200

    Added default scope to todo list

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -5,6 +5,8 @@ class TodoList < ActiveRecord::Base
   has_many :watchings, :foreign_key => "watched_list_id", :dependent => :destroy
   has_many :watchers, :through => :watchings, :source => :watcher
 
+  default_scope order("created_at DESC")
+
   validates :name, :presence => true
   accepts_nested_attributes_for :tasks, :reject_if => lambda { |task| task["name"].blank? }, :allow_destroy  => true
 
