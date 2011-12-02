87697fc00858d1ca94ba1c34a7bf15ee5d3f63b4
Rondy <rondysousa@gmail.com>
2011-12-02 00:33:02 -0200

    Added 'thumb' version for avatar uploader

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -16,6 +16,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   process :resize_to_fill => [32,32]
 
+  version :thumb do
+    process :resize_to_fill => [24,24]
+  end
+
   def extension_white_list
     %w(jpg jpeg gif png)
   end
