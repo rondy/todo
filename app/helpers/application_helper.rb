77b1352e435bee8b7001ede4366924b0568b2536
Rondy <rondysousa@gmail.com>
2011-12-02 00:36:32 -0200

    Moved flash messages to a helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,10 @@ module ApplicationHelper
     render "shared/error_messages", :resource => resource
   end
 
+  def flash_messages
+    flash.collect do |key, message|
+      content_tag :div, message, :id => "flash_#{key}", :class => "flash_message"
+    end.join.html_safe
+  end
+
 end
