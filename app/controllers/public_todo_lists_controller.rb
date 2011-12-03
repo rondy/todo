bb02c11edaa582868ee4f96f391df49303d19630
Rondy <rondysousa@gmail.com>
2011-12-03 16:27:23 -0200

    Public lists page shows all public todo lists except from current user

--- app/controllers/public_todo_lists_controller.rb
+++ app/controllers/public_todo_lists_controller.rb
@@ -1,7 +1,7 @@
 class PublicTodoListsController < TodoListsController
 
   def index
-    @public_todo_lists = TodoList.public_visibility
+    @public_todo_lists = TodoList.public_visibility.except_from_user(current_user)
     respond_with(@public_todo_lists)
   end
 
